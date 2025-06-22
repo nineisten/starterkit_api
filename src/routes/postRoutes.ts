ac92ec6db69599a2d04b059f3e57fcf1ee1b9ad5
nineisten <nineisx@icloud.com>
2025-06-22 02:35:22 -0400

    removed and muted other stuff

--- src/routes/postRoutes.ts
+++ src/routes/postRoutes.ts
@@ -1,18 +1,18 @@
 import { Router } from "express";
-import { 
-    createPost,
-    getPosts,
-    getPostById,
-    updatePost,
-    deletePost 
-} from "../controllers/posts/postController";
+// import { 
+//     createPost,
+//     getPosts,
+//     getPostById,
+//     updatePost,
+//     deletePost 
+// } from "../controllers/posts/postController";
 
-const postRoutes = Router();
+// const postRoutes = Router();
 
-postRoutes.get('/',getPosts)
-postRoutes.get('/:id',getPostById)
-postRoutes.post('/',createPost)
-postRoutes.put('/:id',updatePost)
-postRoutes.delete('/:id',deletePost)
+// postRoutes.get('/',getPosts)
+// postRoutes.get('/:id',getPostById)
+// postRoutes.post('/',createPost)
+// postRoutes.put('/:id',updatePost)
+// postRoutes.delete('/:id',deletePost)
 
-export default postRoutes
+// export default postRoutes
